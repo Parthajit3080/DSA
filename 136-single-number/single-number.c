305fa6993d0544fe8f9f172dd41eda17b4ed9f3c
Parthajit3080 <121250517+Parthajit3080@users.noreply.github.com>
2026-02-20 21:09:53 +0530

    Time: 1084 ms (5.25%) | Memory: 9.3 MB (98.01%) - LeetSync

---
+++ 136-single-number/single-number.c
@@ -0,0 +1,13 @@
+int singleNumber(int* nums, int numsSize) {
+    int i,j,c;
+    for(i=0;i<numsSize;i++){
+        c=0;
+        for(j=0;j<numsSize;j++){
+            if(nums[i]==nums[j])
+            c++;
+        }
+        if(c==1)
+            return nums[i];
+    }
+    return -1;
+};
